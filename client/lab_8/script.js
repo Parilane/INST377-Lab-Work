7d638908464980cfd7739159802d4305c314c584
Parilane <112584129+Parilane@users.noreply.github.com>
2022-11-21 22:49:21 -0500

    comma

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -101,7 +101,7 @@ function markerPlace(array, map) {
 
   array.forEach((item, index) => {
     const {coordinates} = item.geocoded_column_1;
-    L.marker([coordinates[1].coordinates[0]]).addTo(map);
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
     if (index === 0) {
       map.setView([coordinates[1], coordinates[0]], 10);
     }
